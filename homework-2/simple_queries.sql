1b2beef0c7e58a731fea3da60253c68c9548b757
Persiteo <Persiteo@users.noreply.github.com>
2023-08-08 21:05:26 +0300

    Update simple_queries.sql
    spaces removed

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -3,15 +3,12 @@
 SELECT contact_name, country FROM customers
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-
 SELECT order_id, (order_date - shipped_date) FROM orders
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
-
 SELECT contact_name, country FROM customers
 
 -- 4. количество заказов (таблица orders)
-
 SELECT COUNT(*) FROM orders
 
 -- 5. количество стран, в которые отгружался товар (таблица orders, колонка ship_country)
