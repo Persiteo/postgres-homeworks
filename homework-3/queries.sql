2d965201f2e78f9aa57eec92a7fa65638a2aa357
Persiteo <Persiteo@users.noreply.github.com>
2023-08-16 23:43:03 +0300

    Update queries.sql
    Added 1st answer

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,13 +1,12 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-select customers.company_name, CONCAT(first_name, ' ', last_name) as employee 
-from employees
-inner join customers USING(city) 
-where city = 'London' 
-INTERSECT 
-select company_name from shippers
-where company_name = 'United Package'
+SELECT customers.company_name, CONCAT(first_name, ' ', last_name) AS fullname
+FROM orders
+JOIN customers USING(customer_id)
+JOIN employees USING(employee_id)
+JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city=employees.city AND employees.city = 'London' AND shippers.company_name = 'United Package';
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
