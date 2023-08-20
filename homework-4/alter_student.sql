8e020dbda5639e8077b5fa85cc4a99e1b3795955
Persiteo <Persiteo@users.noreply.github.com>
2023-08-20 22:48:41 +0300

    Update alter_student.sql
    6th task

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -21,6 +21,7 @@ Alter table student RENAME birthday TO birth_date;
 Alter table student ALTER column phone SET DATA TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student (first_name, last_name, birth_date, phone) values ('Imya', 'Familiya', '2013-12-11', '999-99-99'), 
+('Imya1', 'Familiya1', '2014-11-10', '999-99-98'), ('Imya2', 'Familiya2', '2015-10-09', '999-99-97')
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
