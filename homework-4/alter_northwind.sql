0938d055f50e14c1d2c41334fe9d394f50e30655
Persiteo <Persiteo@users.noreply.github.com>
2023-08-20 23:03:36 +0300

    Update alter_northwind.sql
    3rd task

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -6,7 +6,7 @@ ALTER TABLE products ADD CONSTRAINT chk_products_unit_price CHECK (unit_price >0
 ALTER TABLE products ADD CONSTRAINT chk_products_discontinued CHECK (discontinued in (0,1));
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
-
+SELECT * INTO discontinued_products from products where discontinued = 1;
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
